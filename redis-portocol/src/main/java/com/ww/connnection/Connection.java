961481e5cbec80fd58b4fa78e8b864a9831a07dc
wwei <429744781@qq.com>
2020-05-07 09:26:17 +0800

    更新

--- redis-portocol/src/main/java/com/ww/connnection/Connection.java
+++ redis-portocol/src/main/java/com/ww/connnection/Connection.java
@@ -1,5 +1,8 @@
 package com.ww.connnection;
 
+import com.ww.protocol.Protocol;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
@@ -10,10 +13,46 @@ import java.net.Socket;
 public class Connection {
     private Socket socket;
     private String host;
-    private String port;
+    private int port;
     private OutputStream outputStream;
     private InputStream inputStream;
-    public void connect(){
-        
+
+    public Connection(String host, int port) {
+        this.port = port;
+        this.host = host;
+    }
+
+    public void connect() {
+        try {
+            socket = new Socket(host, port);
+            outputStream = socket.getOutputStream();
+            inputStream = socket.getInputStream();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * 发送数据
+     *
+     * @param command
+     * @param args
+     * @return
+     */
+    public Connection sendCommand(Protocol.Command command, byte[]... args) {
+        //连接
+        connect();
+        Protocol.sendCommand(outputStream, command, args);
+        return this;
+    }
+
+    public String getReply() {
+        byte[] b = new byte[1024];
+        try {
+            socket.getInputStream().read(b);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new String(b);
     }
 }
